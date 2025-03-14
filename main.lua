959a35f47cc0b3ec0973ac920d459d2bbb3a4305
BuildTools <unconfigured@null.spigotmc.org>
2025-03-13 23:13:24 -0600

    added more sounds

--- main.lua
+++ main.lua
@@ -3,8 +3,16 @@ local mod
 print("loaded file from github!")
 speaker = peripheral.find("speaker")
 
+sounds = {
+  "entity.creeper.primed",
+  "entity.ender_dragon.roar"
+  "entity.enderman.death"
+  "entity.wolf.death"
+}
+
 while true do
-  speaker.playSound("entity.creeper.primed")
+  local sound = sounds[math.random(1, #sounds)]
+  speaker.playSound(sound)
   os.sleep(math.random(1, 10))
 end
 
