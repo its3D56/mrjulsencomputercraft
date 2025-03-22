ad13d556df16bb52208fb6b22dd0c16d532da94c
BuildTools <unconfigured@null.spigotmc.org>
2025-03-22 04:03:59 -0600

    Wrote most of the code for death counter

---
+++ main.lua
@@ -0,0 +1,57 @@
+local strings = require "cc.strings"
+
+local achor_dl = peripheral.find("create_target")
+local monitor = peripheral.find("monitor")
+
+
+function main()
+  local death_counts = {}
+
+  local iterations = 0
+  update_death_counts(death_counts)
+  display_death_counts(death_counts)
+
+  if iterations % 150 == 0 then
+    write_death_counts(death_counts)
+    -- death_counts = read_death_counts()
+  end
+
+  iterations = iterations + 1
+  os.sleep(15)
+  return true
+end
+
+function update_death_counts(death_counts)
+  for i = 1, 24 do
+    local line = anchor_dl.getLine(i)
+    
+    if line.starts(" ") then break end
+    
+    local name, deathcount = parse_line(line)
+    death_counts[name] = deathcount
+  end
+end
+
+function parse_line(ln)
+  local name_end = string.find(ln, " ")  
+  local name = string.sub(ln, 1, name_end - 1)
+  local death_count = tonumber(string.sub(ln, name_end, -1))
+  return name, death_count
+end
+
+function display_death_counts(death_counts)
+  monitor.clear()
+  for name, death_count in pairs(death_counts) do
+    monitor.write(name .. "    " .. death_count .. "\n")
+  end
+end
+
+function write_death_counts(death_counts)
+
+end
+
+function read_death_counts()
+
+end
+
+while main() do end
