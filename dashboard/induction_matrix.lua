cea54888779ec98cbb7c57b2f1786f6f16023eb4
BuildTools <unconfigured@null.spigotmc.org>
2025-03-22 22:10:23 -0600

    switched from math.round to math.floor because cc:tweaked doesnt have a round function for some reason

--- dashboard/induction_matrix.lua
+++ dashboard/induction_matrix.lua
@@ -5,12 +5,12 @@ end)
 function display(monitor)
   local stored_power = j_to_fe(im.getEnergy())
   local capacity = j_to_fe(im.getMaxEnergy())
-  local stored_percentage = math.round(stored_power / capacity * 100)
+  local stored_percentage = math.floor(stored_power / capacity * 1000) / 10
   local input = j_to_fe(im.getLastInput())
   local output = j_to_fe(im.getLastOutput())
   local transfer_cap = j_to_fe(im.getTransferCap())
   monitor.writeLine("INDUCTION MATRIX:")
-  monitor.writeline("  Stored power: " .. stored_power .. "FE / " .. capacity .. "FE ")
+  monitor.writeline("  Stored power: " .. stored_power .. "FE / " .. capacity .. "FE (" .. stored_percentage .. "%)")
   monitor.writeline("  I/O: " .. input .. "FE/t / " .. output .. "FE/t" .. " Cap: " .. transfer_cap .. "FE/t")
 end
 
